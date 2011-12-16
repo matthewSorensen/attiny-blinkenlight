541d0dfae5b89760ba3a3cc615a8f053e96d79c8
Matthew Sorensen <scholl.ma@gmail.com>
2011-12-16 00:10:25 -0800

    cleaning things up

--- attiny85.cc
+++ attiny85.cc
@@ -5,12 +5,12 @@
 #include <avr/eeprom.h> 
 
 
-#define high(pin) (PORTB |=  (1 << pin))
-#define low(pin)  (PORTB &= ~(1 << pin))
+#define RED    (1<<PB0)
+#define GREEN  (1<<PB1)
+#define BLUE   (1<<PB2)
+#define SWITCH (1<<PB3)
+#define SENSOR (1<<PB4)
 
-#define RED   PB0
-#define GREEN PB1
-#define BLUE  PB2
 
 static uint8_t red;
 static uint8_t green;
@@ -31,10 +31,9 @@ void checkAndInitialize(){
     return;
   uint8_t* ptr = (uint8_t*) BLUE_BASE;
   char size = 3*RING_BUFFER_LEN;
-  while(size>0){// 0 out red, blue, green
+  while(size-->0)// 0 out red, blue, green
     eeprom_write_byte(ptr++,0);
-    size--;
-  }// 0 out the status byte
+  // Set the status byte to 0xCC
   eeprom_write_byte((uint8_t*) INIT_FLAG,0xCC);
 }
 void writeColor(uint8_t color, uint8_t* base){
@@ -49,7 +48,7 @@ void writeColor(uint8_t color, uint8_t* base){
     }
     loc++;
   }
-  // Otherwise, nuke the last bit with 0, and write the new value to the begining
+  // Otherwise, nuke the last byte with 0, and write the new value to the begining
   if(0 != eeprom_read_byte(loc))
     eeprom_write_byte(loc,0);
   if(color != 0)
@@ -60,77 +59,79 @@ uint8_t readColor(uint8_t* base){
   uint8_t i = RING_BUFFER_LEN;
   while(i-->0){
     uint8_t color = eeprom_read_byte(base++);
-    if(color != 0){
+    if(color != 0)
       return color;
-    }
   }
   return 0;
 }
 
 
-
-#define F_CPU 16000000UL
-#define Clear(pin) (pinlevelB &= ~_BV(pin))
-#define PORTB_DDR  0b00000111
 #define PORTB_MASK 0b00000111
 
 ISR (TIMER0_COMPA_vect)
 {
-  static uint8_t pinlevelB=PORTB_MASK;
-  static uint8_t softcount=0xFF;
-
-  PORTB = pinlevelB;
-
-  if(++softcount == 0){         /* increment modulo 256 counter and update the compare values only when counter = 0. */
-    pinlevelB = PORTB_MASK;     /* set all affected port pins high */
-  }
-  
-  if(red   == softcount) Clear(RED);
-  if(green == softcount) Clear(GREEN);
-  if(blue  == softcount) Clear(BLUE);
+  static uint8_t output=PORTB_MASK;
+  static uint8_t icount=0xFF;
+  // Output the (potentially new) values
+  PORTB = output;
+  // Every 256 interrupts, put all of the pins back to their initial state.
+  if(++icount == 0)
+    output = PORTB_MASK;    
+  // Periodically turn them off
+  if(red   == icount) output &= ~RED;
+  if(green == icount) output &= ~GREEN;
+  if(blue  == icount) output &= ~BLUE;
 }
 
 void setup()
 {
-  // Initialize pinmodes: Turn pin 0, 1 and 2 to outputs.
-  DDRB = PORTB_MASK;
+  // Initialize pin modes: set pin 0, 1 and 2 to outputs.
+  DDRB = 0b00000111;
   PRR = _BV(PRADC); // Turn off the ADC
-  if(PINB & (1<<PB3)){
+  if(PINB & SWITCH){
     // Enter the rather complex color observation mode, write the new ones out to the EPROM.
-    red = 10; green = 10; blue = 0;
-  }else{
-    // Read the old values from the eeprom, initializing it if needed.
+    // Test code right now.
+    red   = readColor((uint8_t*) RED_BASE)+93;
+    green = readColor((uint8_t*) GREEN_BASE)+121;
+    blue  = readColor((uint8_t*) BLUE_BASE)+37;
+    writeColor(red, (uint8_t*) RED_BASE);
+    writeColor(green, (uint8_t*) GREEN_BASE);
+    writeColor(blue, (uint8_t*) BLUE_BASE);
+  }else{// Read the old values from the eeprom, initializing it if needed.
     checkAndInitialize();
     red   = readColor((uint8_t*) RED_BASE);
     green = readColor((uint8_t*) GREEN_BASE);
     blue  = readColor((uint8_t*) BLUE_BASE);
   }
-  // Setup the timer for PWM, then loop
-  cli();
-  // Setup time0 to fire an interrupt every 75 cycles
-  CLKPR = _BV(CLKPCE); // enable clock prescaler update - this bit must be set before
-  CLKPR = 0;           // altering that register, then set clock speed to max
+  /* Setup timer0 to fire an interrupt every 75 cycles, the enter an infinite loop.
+     All the work is done on the interrupts - once we've figured out what colors to use, the
+     device simply has to PWM them until it's shutoff.
+     Based on Atmel's "AVR136: Low-jitter Multi-channel Software PWM" */
+  cli(); // This is probably not needed. Disable interrupts.
+  /* Interesting design: before altering any other bits in CLKPR, CLKPCE must be set.
+     The processor then resets CLKPCE within four cycles. After we've enabled updating
+     CLKPR, set the clock speed to its maximum. */
+  CLKPR = _BV(CLKPCE);
+  CLKPR = 0;
   TCCR0B = _BV(CS00);  // No prescaler
   TCCR0A = _BV(WGM01);// Clear the timer on compare
-  // interrupt on compare register A
   OCR0A = 75;
-  TIMSK |= _BV(OCIE0A);
-  sei();// enable interrupts, enter an infinite loop - all work is done on interrupts.
+  TIMSK |= _BV(OCIE0A);// interrupt on compare with register A, which just happens to be 75. 
+  sei(); // Turn everything on, and loop!
   while(1);
 }
-
 void loop(){;}
 
 void debug(char c){
   for(char i=0;i<8;i++){
     if(c &1){
-      high(BLUE);
+      PORTB |= BLUE;
       delay(500);
-      low(BLUE);
+      PORTB &= ~BLUE;
     }else{
-      high(RED);
+      PORTB |= RED;
       delay(500);
-      low(RED);
+      PORTB &= ~RED;
     }
     delay(1000);
     c = c>>1;
